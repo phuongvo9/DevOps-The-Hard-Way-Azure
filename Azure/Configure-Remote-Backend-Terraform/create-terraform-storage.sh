b76005866af8ec8abe587e37b930f093163e20bd
phuongvo9 <huyphuong4116@gmail.com>
2021-12-20 16:36:50 +0700

    create storage account

--- Azure/Configure-Remote-Backend-Terraform/create-terraform-storage.sh
+++ Azure/Configure-Remote-Backend-Terraform/create-terraform-storage.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-
-RESOURCE_GROUP_NAME="devopstamops-rg"
-STORAGE_ACCOUNT_NAME="devopstamopssa"
-
+RESOURCE_GROUP_NAME="RG_DevOps_202111220"
+STORAGE_ACCOUNT_NAME="tfbackend01storagesa"
+LOCATION = "southeastasia"
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l $LOCATION -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l $LOCATION --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
+
+
+
+
+# Official CLI https://docs.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az_storage_account_create
