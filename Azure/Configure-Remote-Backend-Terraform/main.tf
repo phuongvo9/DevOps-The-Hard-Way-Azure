515fcaf85979640e80b9eb8984481d2fed83f75a
Phuong VO <47130941+phuongvo9@users.noreply.github.com>
2021-12-21 10:04:43 +0700

    Update main.tf

--- Azure/Configure-Remote-Backend-Terraform/main.tf
+++ Azure/Configure-Remote-Backend-Terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name = "RG_DevOps_20211220"
     storage_account_name = "tfbackend01storagesa"
     container_name = "tfstate-backend"
-    key = "jKKKLJNPYSMP3umElC6sTMPQSawdvxaMj/nTAQMkhHmGWGMNHGI8Uyyo3H30B/raFarF/FdPjoXdc7rnq3SuLg=="
+    key = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   }
 }
 
